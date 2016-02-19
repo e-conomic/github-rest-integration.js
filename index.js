427a64c0beb712d49fecf8d27986a3025629696b
gaggle <mail@jonlauridsen.com>
2016-02-19 16:29:25 +0100

    now exposing a class

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
 "use strict";
+var GitHub = require("github")
+var getSettings = require("./lib/settings")
 
-exports = module.exports = {
-  comment: require("./lib/comment"),
-  pullRequest: require("./lib/pr"),
-  status: require("./lib/status")
+var Manager = function (opts) {
+  this.settings = getSettings(opts)
+
+  Object.defineProperty(this, "github", {
+    get: function () {
+      var self = this
+      if (!this._github) {
+        this._github = new GitHub({version: "3.0.0", timeout: 5000})
+        this._github.authenticate({type: "oauth", token: self.settings.token})
+      }
+      return this._github
+    },
+    set: function (val) {
+      this._github = val
+    }
+  })
 }
+
+var extensions = [
+  {prefix: "pullRequest", module: require("./lib/pr")}
+]
+extensions.forEach(function (data) {
+  Object.defineProperty(Manager.prototype, data.prefix, {
+    get: function () {
+      var self = this
+      var f = {}
+      for (var fName in data.module) {
+        f[fName] = data.module[fName].bind(self)
+      }
+      return f
+    },
+    enumerable: false,
+    configurable: true
+  })
+})
+
+module.exports = Manager
---
+++ test/Manager.test.js
@@ -0,0 +1,32 @@
+"use strict";
+var GitHub = require("github")
+var _ = require("lodash")
+var expect = require("must")
+var s = require("util").format
+var Gri = require("../")
+
+describe("github-rest-integration.js", function () {
+  it("should be a constructor", function () {
+    expect(Gri).to.be.a.constructor()
+  })
+
+  it("should initialize a github object", function () {
+    expect(new Gri({token: "foo"}).github)
+      .to.be.instanceof(GitHub)
+  })
+
+  describe("with instance", function () {
+    var gri
+    beforeEach(function () {
+      gri = new Gri()
+    })
+
+    var methods = ["pullRequest.get"]
+    methods.forEach(function (meth) {
+      it(s("should expose %s", meth), function () {
+        expect(_.get(gri, meth), s("%s not exposed", meth))
+          .to.be.a.function()
+      })
+    })
+  })
+})
